2d139d7e7125fc5af6ffef4bb97a8e609051aa38
cellistigs <ta2507@columbia.edu>
2020-11-24 19:13:25 -0500

    updating setup.

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("../README.md","r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
         name = "configeditor",
         version = "0.0.1",
         author = "Taiga Abe",
         author_email = "ta2507@columbia.edu",
         description = "Package for editing of carcea lab NeuroCAAS config files.", 
-        long_description = long_description,
+        long_description = "Lightweight package to work with config files designed for NeuroCAAS. Allows users to visualize the bounding boxes specified by a given config file on a given video, and edit these bounding boxes with simple interactivity.",
         long_description_content_type = "test/markdown", 
         url = "https://github.com/cellistigs/carcealab_videoconfig",
         packages = setuptools.find_packages(),
